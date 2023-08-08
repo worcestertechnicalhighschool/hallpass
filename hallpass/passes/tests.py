100423bbc61b6f3bb5917bafbdcdd5548d690bd4
Randumb <78370387+RandumbGuy1@users.noreply.github.com>
2023-08-08 08:44:16 -0400

    Added tests and hours to the stopwatch on the student login page

--- hallpass/passes/tests.py
+++ hallpass/passes/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class URLTests(TestCase):
+    def test_testhomepage(self):
+        self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
+        self.client.login(username='test_user_3', password='test_user_3_password')
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_monitorpage(self):
+        self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
+        self.client.login(username='test_user_3', password='test_user_3_password')
+        response = self.client.get(reverse('monitor'))
+        self.assertEqual(response.status_code, 200)
