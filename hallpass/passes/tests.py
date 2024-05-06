820063d0b2a1ca9e1b4b05af4eebe0ba63e19098
GrayJayV0 <131674159+GrayJayV0@users.noreply.github.com>
2024-05-06 13:49:00 -0400

    Navbar finish

--- hallpass/passes/tests.py
+++ hallpass/passes/tests.py
@@ -1,36 +1,30 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.urls import reverse
-from .models import Student, HallPass, Destination, Profile, Building
-from .forms import CreateHallPassForm
+from .models import Student, HallPass, Destination, Profile, Building, Category
 
-class URLTests(TestCase):
-    def test_testhomepage(self):
-        self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
-        self.client.login(username='test_user_3', password='test_user_3_password')
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+# class URLTests(TestCase):
+#     def test_testhomepage(self):
+#         self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
+#         self.client.login(username='test_user_3', password='test_user_3_password')
+#         response = self.client.get('/')
+#         self.assertEqual(response.status_code, 200)
     
-    def test_monitorpage(self):
-        self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
-        self.client.login(username='test_user_3', password='test_user_3_password')
-        response = self.client.get(reverse('monitor'))
-        self.assertEqual(response.status_code, 200)
+#     def test_monitorpage(self):
+#         self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
+#         self.client.login(username='test_user_3', password='test_user_3_password')
+#         response = self.client.get(reverse('monitor'))
+#         self.assertEqual(response.status_code, 200)
 
 from datetime import datetime, timedelta
 class HallPassTests(TestCase):
-    def test_timeout(self):
+    def setUp(self):
+        self.building = Building.objects.create(building = 'test_building')
         self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
-        testStudent = Student('Test','Student','111111')
-        testBR = Destination('TestBr')
-        TestHallPass = HallPass(Time_in = datetime.now(),Time_out =  (datetime.now() + timedelta(minutes = 30)),student_id = testStudent ,destination = testBR,user = self.user)
-        self.assertEqual(TestHallPass.Time_out > TestHallPass.Time_in, True)
+        self.student = Student.objects.create(first_name = 'Test', last_name = 'Student', student_id = '111111', building = self.building)
+        self.category = Category.objects.create(name = 'boys')
+        self.br = Destination.objects.create(room = "testBR", building = self.building, category = self.category, max_people_allowed = 2)
 
-    def test_profile_location(self):
-        self.user = User.objects.create_user('test_user_3', 'randumbguy21@gmail.com', 'test_user_3_password')
-        testHS = Building('WTHS')
-        testStudent = Student('Test','Student','111111')
-        testBR = Destination(building = testHS,room = 'WTHS')
-        testProfile = Profile(user = self.user,building = testHS)
-        TestHallPass = HallPass(Time_in = datetime.now(),Time_out = (datetime.now() + timedelta(minutes = 30)),student_id = testStudent ,destination = testBR,user = testProfile.user)
-        self.assertEqual(TestHallPass.user, False)
+    def test_timein(self):
+        testpass = HallPass.objects.create(student_id = self.student, destination = self.br, user = self.user, time_in = datetime.now())   
+        self.assertTrue(testpass.time_in)
